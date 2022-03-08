ca404ac5d6012467e791f650ca57d2fa2b88fb61
brunoro7 <bruno.ro7c@gmail.com>
2022-03-08 20:33:30 -0300

    entregando requisito 5.

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -2,7 +2,34 @@ import React from 'react';
 import Header from '../components/Header';
 
 class Search extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      inputSearchValue: '',
+      buttonSearchDisable: true,
+    };
+  }
+
+  handleButtonSearch = ({ target: { value } }) => {
+    const valorSearch = value;
+    const numberToCompare = 2;
+    this.setState({
+      inputSearchValue: valorSearch,
+    });
+    if (valorSearch.length >= numberToCompare) {
+      this.setState({
+        buttonSearchDisable: false,
+      });
+    } else {
+      this.setState({
+        buttonSearchDisable: true,
+      });
+    }
+    console.log(value);
+  }
+
   render() {
+    const { inputSearchValue, buttonSearchDisable } = this.state;
     return (
       <div data-testid="page-search">
         <h2>
@@ -11,6 +38,22 @@ class Search extends React.Component {
         <h3>
           <Header />
         </h3>
+        <form>
+          <input
+            type="text"
+            data-testid="search-artist-input"
+            value={ inputSearchValue }
+            onChange={ this.handleButtonSearch }
+          />
+          {' '}
+          <button
+            type="button"
+            data-testid="search-artist-button"
+            disabled={ buttonSearchDisable }
+          >
+            Pesquisar
+          </button>
+        </form>
       </div>
     );
   }
